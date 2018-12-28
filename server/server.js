aa179b67badcf13258b8574193fece765dcccbc6
Shashank Gupta <shashank.gupta@skilrock.com>
2018-12-28 16:51:11 +0530

    asd

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+const {generateMessage} = require("./utils/message");
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -18,15 +18,9 @@ io.on('connection', (socket) => {
         console.log('createMessage', message)
     })
 
-    socket.emit('newMessage', {
-        from: 'server',
-        text: 'welcome to chat room'
-    })
+    socket.emit('newMessage', generateMessage('Admin','welcome to chat room'))
 
-    socket.broadcast.emit('newMessage', {
-        from: 'admin',
-        text: 'new user joined'
-    })
+    socket.broadcast.emit('newMessage', generateMessage('admin','New user joined'))
 
     socket.on('disconnect', () => {
         console.log("Disconnected from client")
